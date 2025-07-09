6be48e9da5e353f2526b507358e885b34b97db7d
LeeEunSan <korean7638@gmail.com>
2025-07-09 15:36:54 +0900

    v0.0.8] feat : 펫 프로필 추가

--- Assets/_Folder/_Les/3.Script/TiemTable_LES.cs
+++ Assets/_Folder/_Les/3.Script/TiemTable_LES.cs
@@ -1,6 +1,17 @@
+using CustomInspector;
+using TMPro;
 using UnityEngine;
 
 public class TiemTable_LES : MonoBehaviour
 {
-    
+    public TextMeshProUGUI Time; //시간 표시용
+
+    //시간 로직
+    public float speed = 60f; // 실제 1초 = 60 게임 시간초
+    private float gameTimeSec;
+
+    public void morningClass()
+    {
+
+    }
 }
