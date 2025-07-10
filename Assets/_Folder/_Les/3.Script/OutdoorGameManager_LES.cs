5ebf11395066daafc6d056941c6ff30a9c78ff06
LeeEunSan <korean7638@gmail.com>
2025-07-10 12:44:27 +0900

    v0.0.10] feat : 시간 작업중

---
+++ Assets/_Folder/_Les/3.Script/OutdoorGameManager_LES.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+public class OutdoorGameManager_LES : MonoBehaviour
+{
+    public static OutdoorGameManager_LES Instance;
+
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        TimeManager_LES.instance.time = text;
+        TimeManager_LES.instance.OutdoorTime();
+    }
+}
