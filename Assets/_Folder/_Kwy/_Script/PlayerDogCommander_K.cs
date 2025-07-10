f9df3927a9acee97acbf0829dad0a7f13898b6ee
janghyeok <janghuck0410@naver.com>
2025-07-10 17:33:33 +0900

    이리와 수정 2

--- Assets/_Folder/_Kwy/_Script/PlayerDogCommander_K.cs
+++ Assets/_Folder/_Kwy/_Script/PlayerDogCommander_K.cs
@@ -5,10 +5,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerDogCommander_K : MonoBehaviour
 {
-    [Header("원거리 명령 (호출)")]
-    public Transform rightHandController; // 레이저를 쏠 컨트롤러
-    public InputActionReference callAction; // '이리와' 명령에 사용할 버튼
-
     [Header("근거리 명령 (선택된 강아지 대상)")]
     public InputActionReference sitAction; // '앉아' 명령에 사용할 버튼
     public InputActionReference lieDownAction; // '엎드려' 명령에 사용할 버튼
@@ -23,32 +19,14 @@ public class PlayerDogCommander_K : MonoBehaviour
 
     private void OnEnable()
     {
-        // 각 입력 액션이 수행될 때(버튼이 눌릴 때) 해당하는 함수를 연결(구독)합니다.
-        callAction.action.performed += CallDog;
         sitAction.action.performed += TryCommandSit;
         lieDownAction.action.performed += TryCommandLieDown;
     }
 
     private void OnDisable()
     {
-        // 오브젝트가 파괴될 때, 연결했던 함수들을 해제(구독 취소)합니다.
-        callAction.action.performed -= CallDog;
         sitAction.action.performed -= TryCommandSit;
         lieDownAction.action.performed -= TryCommandLieDown;
-    }
-
-    private void CallDog(InputAction.CallbackContext context)
-    {
-        if (interactionManager == null) return;
-        RaycastHit hit;
-        if (Physics.Raycast(rightHandController.position, rightHandController.forward, out hit, 50f))
-        {
-            DogFSM_K dog = hit.collider.GetComponent<DogFSM_K>();
-            if (dog != null)
-            {
-                dog.BeCalled(this.transform);
-            }
-        }
     }
 
     private void TryCommandSit(InputAction.CallbackContext context)
