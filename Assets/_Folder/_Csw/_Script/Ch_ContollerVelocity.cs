06fd3a6eddd2eb9b7a080884c7d14268f8059326
Tiha123 <waits6262@gmail.com>
2025-07-18 10:46:59 +0900

    v0.0.16]edit:WetfoodProcessing

--- Assets/_Folder/_Csw/_Script/Ch_ContollerVelocity.cs
+++ Assets/_Folder/_Csw/_Script/Ch_ContollerVelocity.cs
@@ -6,9 +6,12 @@ public class Ch_ContollerVelocity : MonoBehaviour
     public InputActionProperty velocityProperty;
     
     public Vector3 velocity;
+    
+    public float magnitude;
 
     private void FixedUpdate()
     {
         velocity=velocityProperty.action.ReadValue<Vector3>();
+        magnitude=velocity.magnitude;
     }
 }
--- Assets/_Folder/_Csw/_Script/Ch_WetFood.cs
+++ Assets/_Folder/_Csw/_Script/Ch_WetFood.cs
@@ -1,43 +1,54 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Ch_WetFood : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
     [SerializeField] Ch_CannedFood cannedFood;
 
     [Range(0f, -1f), SerializeField] private float upsideDownRange = -0.7f;
     [SerializeField] private Transform startPoint;
+    
     private float duration;
     private bool isOut=false;
+    private Ch_VelocityInteractable interactable;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
         cannedFood = GetComponentInChildren<Ch_CannedFood>();
+        interactable = GetComponent<Ch_VelocityInteractable>();
     }
 
-    private void Update()
+    void Start()
     {
-        if (Vector3.Distance(startPoint.position, cannedFood.endPoint.position) < 0.01f)
-        {
-            isOut=true;
-            cannedFood.OnOut();
-        }
-        Debug.Log(rb.velocity.magnitude);
-        if (transform.up.y < upsideDownRange)
-        {
-            Debug.Log("UpsideDown");
-            if (rb.velocity.magnitude > 0f && !isOut)
-            {
-                duration = 1f / (Mathf.Clamp(rb.velocity.magnitude,1f,5f)*Time.deltaTime);
-                cannedFood.transform.DOLocalMoveY(transform.localPosition.y + 0.001f, duration)
-                    .SetEase(Ease.OutBack);
-            }
-        }
-        else
-        {
+        StartCoroutine(Update_Co());
+    }
 
+    private IEnumerator Update_Co()
+    {
+        while (true)
+        {
+            if (Vector3.Distance(startPoint.position, cannedFood.endPoint.position) < 0.01f)
+            {
+                isOut=true;
+                cannedFood.OnOut();
+                yield break;
+            }
+            if (transform.up.y < upsideDownRange)
+            {
+                if (interactable.velocity.magnitude >= 1f && !isOut)
+                {
+                    duration = 1f / (Mathf.Clamp(interactable.velocity.magnitude,1f,5f)*Time.deltaTime);
+                    yield return cannedFood.transform.DOLocalMoveY(transform.localPosition.y + 0.05f, duration)
+                        .SetEase(Ease.OutBack);
+
+                }
+            }
+            else
+            {
+                cannedFood.transform.DOKill();
+            }
         }
     }
 }
