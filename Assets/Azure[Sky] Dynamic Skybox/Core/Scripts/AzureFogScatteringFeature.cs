e25041f7979926e0928a9f8b2743f6bb89cd73ae
LeeEunSan <korean7638@gmail.com>
2025-07-24 14:14:12 +0900

    v0.0.35] Add : 파티클 에셋 추가

---
+++ Assets/Azure[Sky] Dynamic Skybox/Core/Scripts/AzureFogScatteringFeature.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using UnityEngine.AzureSky;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class AzureFogScatteringFeature : ScriptableRendererFeature
+{
+    public RenderPassEvent renderPassEvent { get => m_renderPassEvent; set => m_renderPassEvent = value; }
+    [SerializeField] private RenderPassEvent m_renderPassEvent = RenderPassEvent.BeforeRenderingSkybox;
+
+    /// <summary>The material that will render the fog scattering RenderTexture into the screen.</summary>
+    public Material fogRendererMaterial { get => m_fogRendererMaterial; set => m_fogRendererMaterial = value; }
+    [SerializeField] private Material m_fogRendererMaterial = null;
+
+    /// <summary>The material that will compute the fog scattering effect and render it into a RenderTexture.</summary>
+    public Material fogComputationMaterial { get => m_fogComputationMaterial; set => m_fogComputationMaterial = value; }
+    [SerializeField] private Material m_fogComputationMaterial = null;
+
+    /// <summary>The render texture that stores the fog scattering data. (RGB: Scattering Data), (Alpha: Fog Data).</summary>
+    public RenderTexture fogScatteringRT { get => m_fogScatteringRT; set => m_fogScatteringRT = value; }
+    private RenderTexture m_fogScatteringRT = null;
+
+    /// <summary>The start width resolution of the fog scattering render texture.</summary>
+    public int fogRenderTextureWidth => m_fogRenderTextureWidth;
+    [SerializeField] private int m_fogRenderTextureWidth = 1920;
+
+    /// <summary>The start height resolution of the fog scattering render texture.</summary>
+    public int fogRenderTextureHeight => m_fogRenderTextureHeight;
+    [SerializeField] private int m_fogRenderTextureHeight = 1080;
+
+    /// <summary>The instance of the AzureFogScatteringPass class.</summary>
+    private AzureFogScatteringPass m_azureFogScatteringPass;
+
+    /// <summary>Called when the renderer feature is created or modified.</summary>
+    public override void Create()
+    {
+        m_azureFogScatteringPass = new AzureFogScatteringPass();
+        m_azureFogScatteringPass.fogRendererMaterial = m_fogRendererMaterial;
+        m_azureFogScatteringPass.fogComputationMaterial = m_fogComputationMaterial;
+
+        SetFogScatteringResolution(m_fogRenderTextureWidth, m_fogRenderTextureHeight);
+        m_azureFogScatteringPass.fogScatteringRT = m_fogScatteringRT;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (fogComputationMaterial == null)
+        {
+            Debug.LogWarningFormat("Missing the Fog Computation Material. {0} blit pass will not execute. Check for missing reference in the assigned renderer.", GetType().Name);
+            return;
+        }
+
+        if (m_fogRendererMaterial == null)
+        {
+            Debug.LogWarningFormat("Missing the Fog Renderer Material. {0} blit pass will not execute. Check for missing reference in the assigned renderer.", GetType().Name);
+            return;
+        }
+
+        m_azureFogScatteringPass.renderPassEvent = m_renderPassEvent;
+        renderer.EnqueuePass(m_azureFogScatteringPass);
+    }
+
+    private class AzureFogScatteringPass : ScriptableRenderPass
+    {
+        /// <summary>The reference to the camera that the fog scattering should be rendered.</summary>
+        public Camera camera { get => m_camera; set => m_camera = value; }
+        private Camera m_camera = null;
+
+        /// <summary>The material that will render the fog scattering RenderTexture into the screen.</summary>
+        public Material fogRendererMaterial { get => m_fogRendererMaterial; set => m_fogRendererMaterial = value; }
+        [SerializeField] private Material m_fogRendererMaterial = null;
+
+        /// <summary>The material that will compute the fog scattering effect and render it into a RenderTexture.</summary>
+        public Material fogComputationMaterial { get => m_fogComputationMaterial; set => m_fogComputationMaterial = value; }
+        [SerializeField] private Material m_fogComputationMaterial = null;
+
+        /// <summary>The render texture that stores the fog scattering data. (RGB: Scattering Data), (Alpha: Fog Data).</summary>
+        public RenderTexture fogScatteringRT { get => m_fogScatteringRT; set => m_fogScatteringRT = value; }
+        [SerializeField] private RenderTexture m_fogScatteringRT = null;
+
+        /// <summary>Stores the camera frunstum corners position.</summary>
+        private Vector3[] m_frustumCorners = new Vector3[4];
+
+        /// <summary>The view port rect.</summary>
+        private Rect m_viewRect = new Rect(0, 0, 1, 1);
+
+        /// <summary>The camera frustum corners matrix.</summary>
+        private Matrix4x4 m_frustumCornersArray = Matrix4x4.identity;
+
+        private RTHandle m_sourceColor;
+        private RTHandle m_DestinationColor;
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            m_sourceColor = renderingData.cameraData.renderer.cameraColorTargetHandle;
+            var desc = renderingData.cameraData.cameraTargetDescriptor;
+            desc.depthBufferBits = 0;
+            RenderingUtils.ReAllocateIfNeeded(ref m_DestinationColor, desc, FilterMode.Point, TextureWrapMode.Clamp, name: "_TemporaryDestinationHandle");
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (m_fogRendererMaterial == null || m_fogComputationMaterial == null) return;
+            m_camera = renderingData.cameraData.camera;
+            if (m_camera == null) return;
+
+            CommandBuffer cmd = CommandBufferPool.Get();
+
+            m_camera.CalculateFrustumCorners(m_viewRect, m_camera.farClipPlane, m_camera.stereoActiveEye, m_frustumCorners);
+            m_frustumCornersArray = Matrix4x4.identity;
+            m_frustumCornersArray.SetRow(0, m_camera.transform.TransformVector(m_frustumCorners[0]));  // bottom left
+            m_frustumCornersArray.SetRow(2, m_camera.transform.TransformVector(m_frustumCorners[1]));  // top left
+            m_frustumCornersArray.SetRow(3, m_camera.transform.TransformVector(m_frustumCorners[2]));  // top right
+            m_frustumCornersArray.SetRow(1, m_camera.transform.TransformVector(m_frustumCorners[3]));  // bottom right
+            m_fogComputationMaterial.SetMatrix(AzureShaderUniforms.FrustumCornersMatrix, m_frustumCornersArray);
+            m_fogRendererMaterial.SetMatrix(AzureShaderUniforms.FrustumCornersMatrix, m_frustumCornersArray);
+
+            // Compute the fog scattering effect and render it into a RenderTexture
+            Graphics.Blit(null, m_fogScatteringRT, m_fogComputationMaterial, 0);
+
+            //Blitter.BlitCameraTexture(cmd, sourceColor, m_DestinationColor, blitMaterial, -1);
+            //Blitter.BlitCameraTexture(cmd, m_DestinationColor, sourceColor);
+
+            //cmd.Blit(m_sourceColor, m_DestinationColor, blitMaterial, -1);
+            //cmd.Blit(m_DestinationColor, m_sourceColor);
+
+            // Render the fog scattering RenderTexture into the screen.
+            m_fogRendererMaterial.SetTexture(AzureShaderUniforms.FogScatteringDataTexture, m_fogScatteringRT);
+            cmd.Blit(m_sourceColor.rt, m_DestinationColor.rt, m_fogRendererMaterial, -1);
+            cmd.Blit(m_DestinationColor.rt, m_sourceColor.rt);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            CommandBufferPool.Release(cmd);
+        }
+    }
+
+    /// <summary>Sets the resolution of the render texture that stores fog scattering data.</summary>
+    public void SetFogScatteringResolution(int width, int height)
+    {
+        m_fogRenderTextureWidth = width;
+        m_fogRenderTextureHeight = height;
+
+        m_fogScatteringRT = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default)
+        {
+            name = "Fog Scattering RT",
+            wrapMode = TextureWrapMode.Clamp,
+            filterMode = FilterMode.Bilinear
+        };
+
+        m_fogScatteringRT.Create();
+    }
+}
---
+++ Assets/_Folder/_Les/3.Script/ParticlePoolManager_LES.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 기분 타입 enum 정의
+public enum MoodType
+{
+    Good,
+    Bad,
+    Happy
+}
+
+public class ParticlePoolManager_LES : MonoBehaviour
+{
+    public static ParticlePoolManager_LES Instance;
+
+    [Header("Particle Settings")]
+    [SerializeField] private GameObject goodParticlePrefab;
+    [SerializeField] private GameObject badParticlePrefab;
+    [SerializeField] private GameObject happyParticlePrefab;
+    
+    [Header("Pool Settings")]
+    [SerializeField] private int poolSize = 10; // 각 타입별 풀 크기
+    [SerializeField] private float particleDuration = 3f; // 파티클 재생 시간
+
+    // 각 MoodType별 파티클 풀
+    private Dictionary<MoodType, Queue<GameObject>> particlePools;
+    private Dictionary<MoodType, GameObject> particlePrefabs;
+
+    private void Awake()
+    {
+        // 싱글톤 패턴
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializePools();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // 파티클 풀 초기화    
+    private void InitializePools()
+    {
+        particlePools = new Dictionary<MoodType, Queue<GameObject>>();
+        particlePrefabs = new Dictionary<MoodType, GameObject>();
+
+        // 프리팹 딕셔너리 설정
+        particlePrefabs[MoodType.Good] = goodParticlePrefab;
+        particlePrefabs[MoodType.Bad] = badParticlePrefab;
+        particlePrefabs[MoodType.Happy] = happyParticlePrefab;
+
+        // 각 MoodType별로 풀 생성
+        foreach (MoodType moodType in System.Enum.GetValues(typeof(MoodType)))
+        {
+            particlePools[moodType] = new Queue<GameObject>();
+            
+            // 풀에 파티클 오브젝트들을 미리 생성하여 추가
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject particle = CreateParticleObject(moodType);
+                particlePools[moodType].Enqueue(particle);
+            }
+        }
+    }
+
+    // 파티클 오브젝트 생성    
+    private GameObject CreateParticleObject(MoodType moodType)
+    {
+        GameObject prefab = particlePrefabs[moodType];
+        if (prefab == null)
+        {
+            Debug.LogError($"Particle prefab for {moodType} is not assigned!");
+            return null;
+        }
+
+        GameObject particle = Instantiate(prefab, transform);
+        particle.SetActive(false);
+        return particle;
+    }
+
+    // 파티클 재생 (위치 지정)    
+    public void PlayParticle(MoodType moodType, Vector3 position)
+    {
+        GameObject particle = GetFromPool(moodType);
+        if (particle != null)
+        {
+            particle.transform.position = position;
+            particle.SetActive(true);
+
+            // 파티클 시스템 재생
+            ParticleSystem ps = particle.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                ps.Play();
+            }
+
+            // 일정 시간 후 풀로 반환
+            StartCoroutine(ReturnToPoolAfterDelay(particle, moodType));
+        }
+    }
+
+    // 파티클 재생 (Transform 지정)    
+    public void PlayParticle(MoodType moodType, Transform target)
+    {
+        PlayParticle(moodType, target.position);
+    }
+
+    // 풀에서 파티클 가져오기    
+    private GameObject GetFromPool(MoodType moodType)
+    {
+        if (particlePools[moodType].Count > 0)
+        {
+            return particlePools[moodType].Dequeue();
+        }
+        else
+        {
+            // 풀이 비어있으면 새로 생성
+            Debug.LogWarning($"Pool for {moodType} is empty. Creating new particle.");
+            return CreateParticleObject(moodType);
+        }
+    }
+
+    // 일정 시간 후 풀로 반환    
+    private IEnumerator ReturnToPoolAfterDelay(GameObject particle, MoodType moodType)
+    {
+        yield return new WaitForSeconds(particleDuration);
+        ReturnToPool(particle, moodType);
+    }
+
+    // 파티클을 풀로 반환    
+    private void ReturnToPool(GameObject particle, MoodType moodType)
+    {
+        if (particle != null)
+        {
+            particle.SetActive(false);
+            particlePools[moodType].Enqueue(particle);
+        }
+    }
+
+    // 모든 파티클 정지    
+    public void StopAllParticles()
+    {
+        foreach (var pool in particlePools.Values)
+        {
+            foreach (var particle in pool)
+            {
+                if (particle.activeInHierarchy)
+                {
+                    ParticleSystem ps = particle.GetComponent<ParticleSystem>();
+                    if (ps != null)
+                    {
+                        ps.Stop();
+                    }
+                    particle.SetActive(false);
+                }
+            }
+        }
+    }
+
+    // 특정 타입 파티클만 정지
+    public void StopParticles(MoodType moodType)
+    {
+        foreach (var particle in particlePools[moodType])
+        {
+            if (particle.activeInHierarchy)
+            {
+                ParticleSystem ps = particle.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Stop();
+                }
+                particle.SetActive(false);
+            }
+        }
+    }
+}
